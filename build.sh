deab8bf1b2eacfe53512d84ee568905e20b5fd44
chanzhennan <chanzhennan@163.com>
2023-11-08 13:28:40 +0800

    fix cmakelist

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf build
 mkdir build && cd build
 cd build
-cmake -DCUDA_ARCH=89 -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-12.1 ..
+cmake -DCUDA_ARCH=80 -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-12.1 ..
 make -j
 ./cuda_benchmark
